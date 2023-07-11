ae98429d0164c3642856e1fda43568157f3adde2
DimitriSouza <dimitri.ses@gmail.com>
2023-07-11 14:32:41 -0300

    Adding lyrics to assignment 3

--- tarefa3.php
+++ tarefa3.php
@@ -9,12 +9,47 @@
 </head>
 <body>
     <h1>Letra da Música</h1>
+    <h2> Paranoid Android - Radiohead</h2>
     <?php
-        echo "Verso 1<br>";
-        echo "Verso 2<br>";
-        echo "Refrão<br>";
-        echo "Verso 3<br>";
-        echo "Refrão<br>";
+        echo "Please, could you stop the noise?<br>
+        I'm trying to get some rest<br>
+        From all the unborn chicken voices<br>
+        In my head<br>";
+        echo "What's that?<br>
+        (I may be paranoid, but not an android)<br>
+        What's that?<br>
+        (I may be paranoid, but not an android)<br>";
+        echo "When I am king<br>
+        You will be first against the wall<br>
+        With your opinion<br>
+        Which is of no consequence at all<br>
+        What's that?<br>
+        (I may be paranoid, but no android)<br>
+        What's that?<br>
+        (I may be paranoid, but no android)<br>";
+        echo "Ambition makes you look pretty ugly<br>
+        Kicking, squealing, Gucci little piggy<br>
+        You don't remember, you don't remember<br>
+        Why don't you remember my name?<br>
+        Off with his head, man, off with his head, man<br>
+        Why don't you remember my name?<br>
+        I guess he does<br>";
+        echo "Rain down, rain down<br>
+        Come on, rain down on me<br>
+        From a great height<br>
+        From a great height, height<br>
+        Rain down, rain down<br>
+        Come on, rain down on me<br>
+        From a great height<br>
+        From a great height, height<br>
+        That's it, sir, you're leaving (Rain down)<br>
+        The crackle of pigskin (Rain down)<br>
+        The dust and the screaming (Come on, rain down)<br>
+        The yuppies networking (On me)<br>
+        The panic, the vomit (From a great height)<br>
+        The panic, the vomit (From a great height)<br>
+        God loves his children<br>
+        God loves his children, yeah <br>";
     ?>
 </body>
 </body>
